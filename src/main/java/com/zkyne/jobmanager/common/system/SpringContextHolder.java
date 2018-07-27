a96f3e72dca298858ffc9e0ed1af699a713d8a62
zhangkun01 <zhangkun01@dangdang.com>
2018-07-27 15:41:32 +0800

    init commit

--- src/main/java/com/zkyne/jobmanager/common/system/SpringContextHolder.java
+++ src/main/java/com/zkyne/jobmanager/common/system/SpringContextHolder.java
@@ -2,6 +2,7 @@ package com.zkyne.jobmanager.common.system;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
 /**
  * @ClassName: SpringContextHolder
@@ -9,6 +10,7 @@ import org.springframework.context.ApplicationContextAware;
  * @Author: zkyne
  * @Date: 2018/7/26 10:27
  */
+@Component
 public class SpringContextHolder implements ApplicationContextAware {
 	private static ApplicationContext applicationContext;
 
